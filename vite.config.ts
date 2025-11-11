32c9d22f22e0fd316044bb2aaf1188384d84bac3
Jaume Anguera Peris <147000713+jaumeAngueraPeris@users.noreply.github.com>
2025-11-11 03:32:16 +0100

    Fix GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: "/jaumeAngueraPeris.github.io",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
